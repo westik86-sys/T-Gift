a739a235a2eb25e8c5fa3eac79b1b18d6f400a9e
e <e>
2026-04-21 13:24:49 +0300

    Initial Commit

---
+++ T-Gift/T_GiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  T_GiftApp.swift
+//  T-Gift
+//
+//  Created by Pavel Korostelev on 21.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct T_GiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
